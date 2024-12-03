82ee3941c15fa692594b1320dbd1043e00c31039
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-12-03 02:14:49 +0200

    Added health check to pipe line

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
-  res.send('ok')
+  res.status(200).send('OK')
 })
 
-app.listen(PORT + 1, () => {
+
+app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
